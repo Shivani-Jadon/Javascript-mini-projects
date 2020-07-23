5f73500a31b8be9e25cc433ede61b08c75730fa7
Shivani <shivanijadon98@gmail.com>
2020-07-23 19:20:02 +0530

    add randomly selected colors

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+console.log("Welcome");
+
+let ball_color = ['red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black', 'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen', 'deepskyblue', 'firebrick', 'crimson']
+
 $("#button-container button").on('click', function(){
     let basket = $("#basket");
     let ball = $("<div></div>");
-    ball.className("collect-ball");
+    
+
+    // randomly selecting color for balls
+    let color_no = Math.floor(Math.random()*ball_color.length);
+    console.log(color_no);
+    ball.css("background-color", ball_color[color_no]);
+    ball.addClass("collect-ball");
     basket.append(ball);
 })
